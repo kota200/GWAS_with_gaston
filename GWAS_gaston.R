0a3eb6d82beb56a9df70ac97ec19e00dc1b806c5
KotaKambara <78218323+kota200@users.noreply.github.com>
2025-07-17 17:16:14 +0900

    Update and rename GWAS_gaston_20250416.R to GWAS_gaston.R

--- GWAS_gaston.R
+++ GWAS_gaston.R
@@ -1,24 +1,24 @@
 #!/usr/bin/env Rscript
-#pcaを上位10グループ、lmmをなし
-#第一引数：VCFファイル,第二引数: 表現型ファイル,第三引数：p-value cutoff,第四引数：output file prefix
-#コマンドライン引数からvcfファイルと表現型ファイルのパスを取得
+#gwas will be performed using a linear mixed model
+#arg1：VCF file, arg2: phenotypic file, arg3：p-value cutoff, arg4：output file prefix
+
 args <- commandArgs(trailingOnly = TRUE)
 
 #library
 library(gaston)
 library(vcfR)
 
-# 引数から値を取得
-vcf_file <- args[1]      # 第一引数: VCFファイル
-pheno_file <- args[2]    # 第二引数: 表現型ファイル
-pvalue <- args[3]	#第三引数：p-value cutoff
-pre <- args[4]	#第四引数：output file prefix
+
+vcf_file <- args[1]      
+pheno_file <- args[2]    
+pvalue <- args[3]	
+pre <- args[4]	
 
 print("----!----")
-print("表現型データの欠損値はNAにしてください")
+print("Make sure that the missing values in your phenotypic file are set as "NA".")
 print("----!----")
 
-#データの読み込み
+
 bm <- read.vcf(vcf_file)
 pheno <- read.table(pheno_file, header = TRUE, row.names = 1)
 
@@ -38,7 +38,7 @@ for(tra in colnames(pheno)){
 	standardize(bm.wp) <- "mu_sigma"
 
 	grm <- GRM(bm.wp)
-#GWASの実行
+#with PCA result
 	gwa <- association.test(bm.wp, Y = y, method = "lmm", response = "quantitative", test = "lrt", eigenK = eigen(grm), p = 10)
 	col <- rep("orange3", nrow(gwa))
 	col[gwa$chr %% 2 == 1] <- "blue4"
@@ -53,7 +53,7 @@ for(tra in colnames(pheno)){
 	a <- a[,c("CHR", "BP", "SNP", "P")]
 	write.csv(a,paste0(pre, "_", tra, "gaston_lmm.gwas"), row.names = FALSE)
 
-#PCA入れないで
+#without PCA result
 	gwa <- association.test(bm.wp, Y = y, method = "lmm", response = "quantitative", test = "lrt", eigenK = eigen(grm), p = 0)
 	col <- rep("orange3", nrow(gwa))
 	col[gwa$chr %% 2 == 1] <- "blue4"
@@ -68,6 +68,4 @@ for(tra in colnames(pheno)){
 	a <- a[,c("CHR", "BP", "SNP", "P")]
 	write.csv(a,paste0(pre, "_", tra, "_gaston_lmm_wo_pca.gwas"), row.names = FALSE)
 }
-
-#最後にコマンドラインで以下を実行するfor i in *.gwas; do sed "s/^/Chr0/g" $i | sed "s/\"//g" | sed "s/Chr0CHR/CHR/g" | sed "s/,/\t/g" > tmp; mv tmp $i; done
 
